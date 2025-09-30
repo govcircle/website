8e81e4cdc759bc5c4612dfef76b970ca072b78d6
Omid <omidajorlou@yahoo.com>
2025-09-30 06:32:53 +0330

    Add GovCircle customizations as overrides in main repository

--- public/docs/js/bundle.js
+++ public/docs/js/bundle.js
@@ -315,7 +315,7 @@ tocBtn.addEventListener('hidden.bs.dropdown', event => {
   var stdin_default;
   var init_stdin = __esm({
     "ns-hugo-params:<stdin>"() {
-      stdin_default = { langPath: "http://localhost:6001/docs/js/components/" };
+      stdin_default = { langPath: "http://localhost:1313/docs/js/components/" };
     }
   });
 
